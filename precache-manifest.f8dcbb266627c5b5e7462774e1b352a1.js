17d4f0c141de8c1260c2cdf431f0e239d8523113
sara maldonado <saramaldonado@gmail.com>
2019-05-07 14:32:54 -0300

    Updates

---
+++ precache-manifest.f8dcbb266627c5b5e7462774e1b352a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9015e16f71cf95aac6df",
+    "url": "/knittingcounter/static/css/main.95067888.chunk.css"
+  },
+  {
+    "revision": "9015e16f71cf95aac6df",
+    "url": "/knittingcounter/static/js/main.09d2d8da.chunk.js"
+  },
+  {
+    "revision": "3d5480f7293f68db0690",
+    "url": "/knittingcounter/static/js/runtime~main.5139e44a.js"
+  },
+  {
+    "revision": "5df8e17f361d4dd73b48",
+    "url": "/knittingcounter/static/css/2.f8d27406.chunk.css"
+  },
+  {
+    "revision": "5df8e17f361d4dd73b48",
+    "url": "/knittingcounter/static/js/2.a5a1b778.chunk.js"
+  },
+  {
+    "revision": "0cfb6f1653d2c5401d114b9cde47ad24",
+    "url": "/knittingcounter/static/media/logo.0cfb6f16.svg"
+  },
+  {
+    "revision": "fbf71c79c7f96f9099a6c5e310d817a8",
+    "url": "/knittingcounter/index.html"
+  }
+];
